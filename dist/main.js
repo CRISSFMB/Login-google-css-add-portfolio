37aa83e6de3f53bd127d10853e931f44b6266bbf
cristian mosquera <cmosqbone1@gmail.com>
2021-07-20 20:09:59 +0200

    navbar finish

--- dist/main.js
+++ dist/main.js
@@ -2,45 +2,6 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/barbarian.js":
-/*!**************************!*\
-  !*** ./src/barbarian.js ***!
-  \**************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "barbarian": () => /* binding */ barbarian
-/* harmony export */ });
-var barbarian = "Hjulmar";
-
-/***/ }),
-
-/***/ "./src/getClasses.js":
-/*!***************************!*\
-  !*** ./src/getClasses.js ***!
-  \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
-/* harmony export */ });
-/* harmony import */ var _wizard__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./wizard */ "./src/wizard.js");
-/* harmony import */ var _barbarian__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./barbarian */ "./src/barbarian.js");
-
-
-
-function getClasses() {
-  console.log("get classes was called");
-  console.log(_wizard__WEBPACK_IMPORTED_MODULE_0__.wizard);
-  console.log(_barbarian__WEBPACK_IMPORTED_MODULE_1__.barbarian);
-}
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getClasses);
-
-/***/ }),
-
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -49,42 +10,8 @@ function getClasses() {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/styles.scss */ "./src/scss/styles.scss");
-/* harmony import */ var _getClasses__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./getClasses */ "./src/getClasses.js");
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-
-
-(0,_getClasses__WEBPACK_IMPORTED_MODULE_1__.default)();
-var elvenShieldRecipe = {
-  leatherStrips: 2,
-  ironIngot: 1,
-  refinedMoonstone: 4
-}; // ES7 Object spread example
-
-var elvenGauntletsRecipe = _objectSpread(_objectSpread({}, elvenShieldRecipe), {}, {
-  leather: 1,
-  refinedMoonstone: 1
-});
-
-console.log("ES7 Object spread example: ", elvenGauntletsRecipe);
-
-/***/ }),
-
-/***/ "./src/wizard.js":
-/*!***********************!*\
-  !*** ./src/wizard.js ***!
-  \***********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "wizard": () => /* binding */ wizard
-/* harmony export */ });
-var wizard = "Ravalynn";
+console.log("hola");
 
 /***/ }),
 
@@ -126,23 +53,6 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
